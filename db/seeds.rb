2f70efd3489a8a4a5b262bbdd351e1277e69dbea
Gala Thevenet <galathevenet@hotmail.com>
2022-02-17 14:35:54 +0100

    DB seeded with 20 films from api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying db..."
+Movie.destroy_all
+puts "done destroying everything... MUHAHAHA"
+puts "downloading Movies from far far away...."
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)['results']
+
+movies.each do |movie|
+Movie.create!(
+  title: movie['original_title'],
+  overview: movie['overview'],
+  poster_url: movie['poster_path'],
+  rating: movie['vote_average'])
+end
+puts "donwloading done ! enjoy :)"
