c19691fded086d8005866400e35e4787be021c38
Gala Thevenet <galathevenet@hotmail.com>
2022-02-17 12:28:16 +0100

    validations for 3 models created

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates :movie_id, presence: true, uniqueness: true
+  validates :list_id, presence: true, uniqueness: true
+  validates :comment, length: { minimum: 6 }
 end
