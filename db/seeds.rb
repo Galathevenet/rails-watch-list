33d41f0fb3e87a6319c8e39eb54969471cba95b8
Gala Thevenet <galathevenet@hotmail.com>
2022-02-17 14:42:37 +0100

    DB seeded again with correct img url

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
-puts "destroying db..."
+puts 'destroying db...'
 Movie.destroy_all
-puts "done destroying everything... MUHAHAHA"
-puts "downloading Movies from far far away...."
+puts 'done destroying everything... MUHAHAHA'
+puts 'downloading Movies from far far away....'
 
-url = "http://tmdb.lewagon.com/movie/top_rated"
+url = 'http://tmdb.lewagon.com/movie/top_rated'
 movie_serialized = URI.open(url).read
 movies = JSON.parse(movie_serialized)['results']
 
 movies.each do |movie|
-Movie.create!(
-  title: movie['original_title'],
-  overview: movie['overview'],
-  poster_url: movie['poster_path'],
-  rating: movie['vote_average'])
+  Movie.create!(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
 end
-puts "donwloading done ! enjoy :)"
+puts 'donwloading done ! enjoy :)'
