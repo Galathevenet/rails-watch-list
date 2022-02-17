5a219b10c9d491396019a2ce3e7402d280215a2f
Gala Thevenet <galathevenet@hotmail.com>
2022-02-17 18:17:21 +0100

    bookmarks mvc almost done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,13 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(params_bookmark)
+  end
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment)
 end
